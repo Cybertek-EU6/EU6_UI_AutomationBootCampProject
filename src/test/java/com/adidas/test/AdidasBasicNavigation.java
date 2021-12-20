002d3f64821f14b7d1c8a9d0df50b650dfea877e
OscarSah <oscar@cybertekschool.com>
2021-12-20 19:32:24 +0300

    New steps added

--- src/test/java/com/adidas/test/AdidasBasicNavigation.java
+++ src/test/java/com/adidas/test/AdidasBasicNavigation.java
@@ -1,6 +1,7 @@
 package com.adidas.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,23 +10,48 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class AdidasBasicNavigation {
 
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup(); // this statement sets up my driver
+        // prepare the driver
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); // create and open the browser
 
-        driver.get("https://www.demoblaze.com/index.html");
+        driver.manage().window().maximize();
 
-        WebElement locatorLaptop = driver.findElement(By.xpath("//a[.='Laptops']"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        WebDriverWait wait = new WebDriverWait(driver,10);
+        driver.get("https://www.demoblaze.com/index.html"); // is used to navigate
 
-        wait.until(ExpectedConditions.elementToBeClickable(locatorLaptop));
+      //  WebElement locatorLaptop = driver.findElement(By.xpath("//a[.='Laptops']"));
+        WebElement locatorLaptop = driver.findElement(By.linkText("Laptops"));
+
+        WebDriverWait wait = new WebDriverWait(driver,10); // Explicit wait
+
+    //    wait.until(ExpectedConditions.elementToBeClickable(locatorLaptop));
 
         locatorLaptop.click();
+
+    //    wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Sony vaio i5")));
+
+        driver.findElement(By.partialLinkText("Sony vaio i5")).click();
+
+        driver.findElement(By.linkText("Add to cart")).click();
+
+
+        // the alert does not show up immediately
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        // special class from selenium
+
+        Alert alert = driver.switchTo().alert();
+
+
+        alert.accept();
 
 
         String title = driver.getTitle();
